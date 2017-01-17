0421b8eb8ed850c07eecc823a646152f9f256d12
Thomas Nguyen <V7XH3@regions.com>
2017-01-17 07:35:58 -0600

    for assignment

--- index.js
+++ index.js
@@ -1,33 +1,23 @@
 //	Visit localhost:3000 to view readout from the server
-
-/*
-*	Require the http module from node.js
-*	Listen on port 3000
-*/
 const http = require('http');
 const port = 3000;
 
-
-/*
-*	Log the request that is made to the server
-*	Provide a response from the server to client
-*/
 const requestHandler = (request, response) => {
-	console.log(request.url);
-	const info = (request.connection.remoteAddress);
-	response.end(`Your IP address is ${info}`);
+	http.get('http://jsonplaceholder.typicode.com/users?id=1', (res) => {
+		res.on('data', (d) => {
+			//process.stdout.write(d);
+			response.end(d);
+		});
+	}).on('error', (e) => {
+		console.error(e);
+	});
 }
+
 const server = http.createServer(requestHandler);
 
-
-/*
-*	Error handling:
-*	If error, log error message
-*	If no error, log the port that the server is listening to
-*/
 server.listen(port, (err) => {
 	if (err) {
 		return console.log('Error, check your stuff!', err);
 	}
-	console.log(`Server is listening on ${port}`);  // Log port that the server is listening to
+	console.log(`Server is listening on ${port}`);
 })
